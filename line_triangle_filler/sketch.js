6d5de07625724afa304ea8dcff51ee425b520a9c
thedoodler <ramprakashsmail@gmail.com>
2020-11-07 13:26:47 +0530

    added line skeleton

--- line_triangle_filler/sketch.js
+++ line_triangle_filler/sketch.js
@@ -1,4 +1,7 @@
-
+var array_length;
+var array_height;
+var triangle_base = 20;
+var triangle_height = 20;
 
 function reportsize() {
   canvas.resizeCanvas(window.innerWidth,window.innerHeight);
@@ -10,7 +13,11 @@ function reportsize() {
 function setup(){
   ww = windowWidth;
   wh = windowHeight;
+  x_count = ww/triangle_base;
+  y_count = wh/triangle_height;
   canvas = createCanvas(ww,wh);
+  canvas.position(0,0);
+  canvas.style('z-index','-1');
 
 }
 
